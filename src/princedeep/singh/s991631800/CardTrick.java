5c799aa89a054693c3ad15dbfcc4f277eca9e43c
prince <prince@DESKTOP-O6FNPS0>
2021-06-05 18:12:46 +0530

    Creating scanner object to input value of card and suit and validating.

--- src/princedeep/singh/s991631800/CardTrick.java
+++ src/princedeep/singh/s991631800/CardTrick.java
@@ -1,5 +1,6 @@
 package princedeep.singh.s991631800;
 import java.util.Random;
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
@@ -21,8 +22,22 @@ public class CardTrick {
             c.setValue(random.nextInt(13)+1);
             c.setSuit(Card.SUITS[random.nextInt(4)]);
             magicHand[a]= c;//giving value of ith card of array to c
-            
-           
+               
+        }
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the value of cards");
+        int value= input.nextInt();
+        if ( value < 1 || value >= 14)
+        {
+            System.out.println("Invalid!!! Please enter the value again between 1 and 13");
+            value= input.nextInt();
+        }
+        System.out.println("Enter the suits");
+        String suit = input.next();
+        if(suit!="Spades"&& suit!="Hearts" && suit!="Diamonds" && suit!="Clubs")
+        {
+            System.out.println("Invalid!!! Please enter the suits again");
+            suit= input.nextLine();
         }
         
     }
